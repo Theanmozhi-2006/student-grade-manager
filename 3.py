3c7eadf8f825f619b4afe6ca883e92e84a6744b7
Theanmozhi <theanmozhidhanasekaran@gmail.com>
2026-06-03 21:16:40 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,48 @@
+students = {}
+
+def add_student(name, mark):
+    # add name and mark to students dictionary
+    students[name] = mark
+
+def view_students():
+    print(students)
+
+def highest_scorer():
+    name=max(students, key=lambda x: students[x])
+    print(name, students[name])
+
+def lowest_scorer():
+    name = min(students, key=lambda x: students[x])
+    print(name, students[name])
+
+def class_average():
+    print(sum(students.values())/len(students))
+
+
+def menu():
+    while True:
+        print("\n--- Student Grade Manager ---")
+        print("1. Add Student")
+        print("2. View All Students")
+        print("3. Highest Scorer")
+        print("4. Lowest Scorer")
+        print("5. Class Average")
+        print("6. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            name = input("Enter name: ")
+            mark = int(input("Enter mark: "))
+            add_student(name, mark)
+        elif choice == "2":
+            view_students()
+        elif choice == "3":
+            highest_scorer()
+        elif choice == "4":
+            lowest_scorer()
+        elif choice == "5":
+            class_average()
+        elif choice == "6":
+            break
+menu()
